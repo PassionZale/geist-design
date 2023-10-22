4d58efc59d968fe8739c1bebee7327b783a309b2
Zhang Lei <372470745@qq.com>
2023-10-22 10:17:28 +0800

    doc: sidebar

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -1,37 +1,31 @@
 export const sidebar = {
-  '/guide/': {
-    base: '/guide/',
-    items: [
-      {
-        text: '快速开始',
-        items: [
-          { text: '什么是 Geist Design', link: 'introduction' },
-          { text: '安装', link: 'installation' }
-        ]
-      },
-      {
-        text: '定制与主题',
-        items: [
-          { text: '主题', link: 'themes' },
-          { text: '色彩', link: 'colors' },
-          { text: '文字排版', link: 'typography' },
-          { text: '图标', link: 'icons' }
-        ]
-      }
-    ]
-  },
-  '/components/': {
-    base: '/components/',
-    items: [
-      {
-        text: '组件',
-        items: [
-          { text: 'Avatar', link: 'avatar' },
-          { text: 'Button', link: 'button' },
-          { text: 'Card', link: 'card' },
-          { text: 'Code', link: 'code' },
-        ]
-      }
-    ]
-  }
+  '/guide/': [
+    {
+      text: '快速开始',
+      items: [
+        { text: '什么是 Geist Design', link: '/guide/introduction' },
+        { text: '安装', link: '/guide/installation' }
+      ]
+    },
+    {
+      text: '定制与主题',
+      items: [
+        { text: '主题', link: '/guide/themes' },
+        { text: '色彩', link: '/guide/colors' },
+        { text: '文字排版', link: '/guide/typography' },
+        { text: '图标', link: '/guide/icons' }
+      ]
+    }
+  ],
+  '/components/': [
+    {
+      text: '组件',
+      items: [
+        { text: 'Avatar', link: '/components/avatar' },
+        { text: 'Button', link: '/components/button' },
+        { text: 'Card', link: '/components/card' },
+        { text: 'Code', link: '/components/code' }
+      ]
+    }
+  ]
 }
