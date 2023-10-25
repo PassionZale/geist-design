974b68eb1b78bb7211a77866846090fd821c8593
PassionZale <whouu@qq.com>
2023-10-25 09:37:02 +0800

    doc: badge props

--- src/badge/src/props.ts
+++ src/badge/src/props.ts
@@ -4,10 +4,19 @@ import { setBooleanProp, setStringNumberProp, setStringProp } from '../../_utils
 export type BadgeStatus = 'success' | 'warning' | 'error'
 
 export const Props = {
+  /** 显示的数字 */
   count: setStringNumberProp(),
+  /** 最大值 */
   maxCount: setStringNumberProp(),
+  /** 是否显示 0 */
   showZero: setBooleanProp(),
+  /** 以圆点的方式显示 */
   dot: setBooleanProp(),
+  /**
+   * 组件状态
+   * 
+   * @values success warning error
+   */
   status: setStringProp()
 } as const
 
