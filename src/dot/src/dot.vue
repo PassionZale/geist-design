94b3eeceda8acce1a4b123ff396876ea96da579e
Zhang Lei <372470745@qq.com>
2023-10-25 20:25:43 +0800

    feat: <GDot />

--- src/dot/src/dot.vue
+++ src/dot/src/dot.vue
@@ -7,5 +7,10 @@
 </script>
 
 <template>
-  <div>GDot</div>
+  <span class="g-dot-wrapper">
+    <span class="g-dot" :class="[type]"></span>
+    <span v-if="$slots.default || label" class="g-dot-label">
+      <slot>{{ label }}</slot>
+    </span>
+  </span>
 </template>
--- src/dot/src/props.ts
+++ src/dot/src/props.ts
@@ -1,6 +1,17 @@
 import type { ExtractPropTypes } from 'vue'
-import {} from '../../_utils'
+import { setStringProp } from '../../_utils'
 
-export const Props = {} as const
+export type DotTypes = 'primary' | 'danger' | 'success' | 'warning'
+
+export const Props = {
+  /**
+   * 点的类型
+   * 
+   * @values primary danger success warning
+   */
+  type: setStringProp<DotTypes>('primary'),
+  /** 标签文字 */
+  label: setStringProp()
+} as const
 
 export type DotProps = ExtractPropTypes<typeof Props>
