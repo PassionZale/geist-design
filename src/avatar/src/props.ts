49799dc984f70b9605c81b7ae405fffcb5c343fc
PassionZale <whouu@qq.com>
2023-10-25 09:24:30 +0800

    doc: avatar props

--- src/avatar/src/props.ts
+++ src/avatar/src/props.ts
@@ -6,10 +6,23 @@ export type AvatarSize = 'small' | 'medium' | 'big' | 'huge'
 export type AvatarShape = 'circle' | 'square'
 
 export const Props = {
+  /** 堆叠显示头像 */
   stacked: setBooleanProp(),
+  /** 图片的 src 属性 */
   src: setStringProp(),
+  /** 当缺少 src 时显示的文字 */
   text: setStringProp(),
+  /**
+   * 头像大小
+   * 
+   * @values small medium big huge
+   */
   size: setStringProp<AvatarSize>('medium'),
+  /**
+   * 组件形状
+   * 
+   * @values circle square
+   */
   shape: setStringProp<AvatarShape>('circle')
 } as const
 
