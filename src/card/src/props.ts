067991837e6cefbdef84c316074e11bc7f7f270f
PassionZale <whouu@qq.com>
2023-10-25 09:37:08 +0800

    doc: card props

--- src/card/src/props.ts
+++ src/card/src/props.ts
@@ -2,7 +2,9 @@ import type { ExtractPropTypes } from 'vue'
 import { setBooleanProp } from '../../_utils'
 
 export const Props = {
+  /** 打开 hover 效果 */
   hoverable: setBooleanProp(),
+  /** 打开 shadow 效果 */
   shadow: setBooleanProp()
 } as const
 
