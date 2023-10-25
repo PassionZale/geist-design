86cecf94b8db60f542f8b1d09632a6aa38c40c1c
PassionZale <whouu@qq.com>
2023-10-25 09:37:15 +0800

    doc: code props

--- src/code/src/props.ts
+++ src/code/src/props.ts
@@ -2,8 +2,11 @@ import type { ExtractPropTypes } from 'vue'
 import { setBooleanProp } from '../../_utils'
 
 export const Props = {
+  /** 显示 `shell` 代码 */
   bash: setBooleanProp(),
+  /** 显示代码块 */
   block: setBooleanProp(),
+  /** 强制单行显示 */
   inline: setBooleanProp()
 } as const
 
