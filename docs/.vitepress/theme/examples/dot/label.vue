eb91053978403959074bba77c9d5c3dc4ee9ea3d
Zhang Lei <372470745@qq.com>
2023-10-25 20:30:43 +0800

    doc: <GDot />

---
+++ docs/.vitepress/theme/examples/dot/label.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  defineOptions({ name: 'ex-dot-label' })
+</script>
+
+<template>
+  <g-dot>primary</g-dot>
+  <br />
+  <br />
+
+  <g-dot type="success">success</g-dot>
+  <br />
+  <br />
+
+  <g-dot type="danger">danger</g-dot>
+  <br />
+  <br />
+
+  <g-dot type="warning" label="warning" />
+</template>
