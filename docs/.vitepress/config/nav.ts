1a96f719e4b867f21332de429d1fabca39d7647a
PassionZale <whouu@qq.com>
2023-11-24 09:08:24 +0800

    config: 导航增加教程

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -12,6 +12,10 @@ export const nav = [
     link: '/components/button'
   },
   {
+    text: '教程',
+    link: 'https://www.lovchun.com/tags/vuejs'
+  },
+  {
     text: `v${version}`,
     link: `https://www.npmjs.com/package/${name}?activeTab=versions`
   }
